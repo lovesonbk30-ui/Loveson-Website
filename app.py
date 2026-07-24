629b12c76cb6636044d91a19ec920a684be77a4a
lovesonbk30-ui <lovesonbk30@gmail.com>
2026-07-24 17:51:49 +0545

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=True)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
 
 
 class Loveson(db.Model):
